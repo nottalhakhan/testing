2ae7f0b5cd4927afec06d0aee295ebd10b421b1d
nottalhakhan <muhammadtalhaasghar791@gmail.com>
2023-12-05 17:31:27 +0500

    V1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+@php
+$a = 'hello';
+echo $a;
+$x = 30;
+@endphp
+@if ($x > 10)
+{{'loop'}}
+@endif
+@while)
+<h1>{{'Loop run'}}</h1>
+@endwhile
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
